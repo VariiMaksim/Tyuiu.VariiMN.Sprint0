d72c9d7c31b1cb9b716947cfe88d021f12c3f31f
Tyuiu.VariiMN@outlook.com <maks.varii@mail.ru>
2025-09-15 14:49:04 +0500

    Task6

---
+++ Tyuiu.VariiMN.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.VariiMN.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.VariiMN.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arraynums = new int[] { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(arraynums));
+            Console.WriteLine("Разность элемента массива = " + DataService.SubtractionArray(arraynums));
+            Console.WriteLine("Произведение элемента массива = " + DataService.MultiplicationArray(arraynums));
+
+            Console.ReadKey();
+        }
+    }
+}
